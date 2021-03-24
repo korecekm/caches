a0f22d8cfc2514af10ac9a8fdfb64ccbe4b66e3d
korecekm <martin.korecek.100@seznam.cz>
2021-03-24 22:22:42 +0100

    LRU Benchmark: A bench measuring the number of cache misses rather than elapsed time
    It's to be taken with a grain of salt. Proper load simulation and query scheduling is pretty much nonexistent

--- benches/lru_bench.rs
+++ benches/lru_bench.rs
@@ -1,6 +1,11 @@
+extern crate caches;
+extern crate criterion;
+extern crate rand;
+
 use caches::lru::LRUCache as LRU;
 use criterion::*;
 use rand::{thread_rng, Rng};
+use std::ptr;
 use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
@@ -14,44 +19,57 @@ const INSERT_FREQ: usize = 20;
 const KEY_OVERFLOW: usize = 2000;
 const KEY_MAX: usize = 15000; // the (non-inclusive) maximum for generated keys (minimum is 0)
 
-pub fn miss_bench(c: &mut Criterion) {
-    let miss_count = Mutex::new(0);
-    let cache = Mutex::new(LRU::new(CACHE_CAPACITY));
-    for k in 0..INIT_RECORD_COUNT {
-        (*cache.lock().unwrap()).insert(k, ());
-    }
+static mut MISS_COUNT: *const Mutex<u64> = ptr::null();
+static mut CACHE: *const Mutex<LRU<usize, ()>> = ptr::null();
 
-    c.bench_function("lru_misses", move |b| {
-        b.iter_custom(|iters| {
-            let mut handles = Vec::new();
-            for _ in 0..iters {
-                let join_handle = thread::spawn(|| {
-                    black_box(perform_miss_bench(&cache, &miss_count));
-                });
-                handles.push(join_handle);
-            }
-            for handle in handles {
-                handle.join();
-            }
-            Duration::from_secs(*miss_count.lock().unwrap())
-        })
-    });
+pub fn miss_bench(c: &mut Criterion) {
+    c.bench_function("lru_misses", prepare_miss_bench);
 }
 
 criterion_group!(miss, miss_bench);
 criterion_main!(miss);
 
-fn perform_miss_bench(cache: &Mutex<LRU<usize, ()>>, miss_count: &Mutex<u64>) {
+fn prepare_miss_bench(b: &mut Bencher) {
+    let miss_count = Mutex::new(0);
+    let cache = Mutex::new(LRU::new(CACHE_CAPACITY));
+    unsafe {
+        MISS_COUNT = &miss_count;
+        CACHE = &cache;
+    }
+    for k in 0..INIT_RECORD_COUNT {
+        (*cache.lock().unwrap()).insert(k, ());
+    }
+
+    b.iter_custom(|iters| {
+        let mut handles = Vec::new();
+        for _ in 0..iters {
+            let join_handle = thread::spawn(|| {
+                black_box(perform_miss_bench());
+            });
+            handles.push(join_handle);
+        }
+        for handle in handles {
+            handle.join().unwrap();
+        }
+        Duration::from_secs(*miss_count.lock().unwrap())
+    });
+}
+
+fn perform_miss_bench() {
     let mut rng = thread_rng();
     for i in 0..QUERY_COUNT {
         let key = rng.gen_range(0, KEY_MAX + KEY_OVERFLOW) % KEY_MAX;
         if i % INSERT_FREQ == 0 {
             // insert
-            (*cache.lock().unwrap()).insert(key, ());
+            unsafe {
+                (*(*CACHE).lock().unwrap()).insert(key, ());
+            }
         } else {
             // get
-            if (*cache.lock().unwrap()).try_get(&key).is_none() {
-                *miss_count.lock().unwrap() += 1;
+            unsafe {
+                if (*(*CACHE).lock().unwrap()).try_get(&key).is_none() {
+                    *(*MISS_COUNT).lock().unwrap() += 1;
+                }
             }
         }
     }
